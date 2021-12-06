7b756c822ce34db517a1d15fba0307feb21aa4f1
sattaku1538 <choose.chose.chosen@gmail.com>
2021-12-06 03:46:39 +0000

    update Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  namespace :public do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    
+  end
+  
+  namespace :public do
+    root to: 'homes#top'
+    get 'homes/about'
+    # ↓いいね、コメント機能
+    resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :update]
+    # resource :favorites, only: [:create, :destroy]
+    # resources :book_comments, only: [:create, :destroy]
+  end
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {registrations: "public/registrations", sessions: 'public/sessions'}
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {sessions: "admin/sessions"}
+  
 end
