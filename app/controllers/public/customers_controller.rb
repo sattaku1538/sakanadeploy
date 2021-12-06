e5ea1f492f86c65fa3ba3efd38f279913f920f49
sattaku1538 <choose.chose.chosen@gmail.com>
2021-12-06 04:12:34 +0000

    update controller customers

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,37 @@
+class Public::CustomersController < ApplicationController
+   before_action :set_customer
+
+    def set_customer
+    @customer = current_customer
+    end
+
+    def show
+	  end
+
+  	def edit
+  	end
+
+  	def update
+  	   if @customer.update(customer_params)
+  	  	redirect_to public_customers_path
+  	  	# フラッシュメッセージいれる？notice: "You have updated your account successfully."
+  	   else
+  	     render "edit"
+         end
+    end
+
+	  def unsubscribe
+    end
+
+	  def withdraw
+    	@customer.update(is_deleted: true)
+    	reset_session
+    	redirect_to public_root_path
+	   end
+
+	  private
+
+	  def customer_params
+		params.require(:customer).permit(:email, :introduction, :name, :profile_image, :is_deleted)
+    end
+end
