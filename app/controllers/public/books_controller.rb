3287b2add4a5a7f02ecd061673908f33dc4bb1cb
sattaku1538 <choose.chose.chosen@gmail.com>
2021-12-09 23:52:16 +0000

    finish IINE Ranking

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -7,18 +7,18 @@ class Public::BooksController < ApplicationController
     @book = Book.find(params[:id])
     # @book_comment = BookComment.new
   end
-
+  
   def index
+  #   # 投稿したものを表示する。
     @books = Book.all
-    # 投稿したものを表示する。
-    # @books = Book.all.sort {|a,b| b.favorites.count <=> a.favorites.count} いいね数の順番に投稿を表示。
-    # to  = Time.current.at_end_of_day
-    # from  = (to - 6.day).at_beginning_of_day
-    # @books = Book.includes(:favorited_users).
-    #   sort {|a,b| 
-    #     b.favorited_users.includes(:favorites).where(created_at: from...to).size <=> 
-    #     a.favorited_users.includes(:favorites).where(created_at: from...to).size
-    #   }
+  #   # ↓↓いいね数の順番に投稿を表示。
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_customers).
+      sort {|a,b| 
+        b.favorited_customers.includes(:favorites).where(created_at: from...to).size <=> 
+        a.favorited_customers.includes(:favorites).where(created_at: from...to).size
+      }
   end
 
   def create
