ed436cfdf59d0ef892fea87b907a5069141fc680
sattaku1538 <choose.chose.chosen@gmail.com>
2021-12-09 03:39:26 +0000

    start IINE

--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,12 @@ Rails.application.routes.draw do
     
   end
   
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {registrations: "public/registrations", sessions: 'public/sessions'}
-  
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {sessions: "admin/sessions"}
+    # 顧客用
+    # URL /customers/sign_in ...
+    devise_for :customers,skip: [:passwords], controllers: {registrations: "public/registrations", sessions: 'public/sessions'}
+    
+    # 管理者用
+    # URL /admin/sign_in ...
+    devise_for :admin, skip: [:registrations, :passwords] ,controllers: {sessions: "admin/sessions"}
   
 end
