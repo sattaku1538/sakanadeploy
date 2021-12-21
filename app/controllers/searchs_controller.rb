ee11daf9388acd80285c84d115edb40a5fef0078
sattaku1538 <choose.chose.chosen@gmail.com>
2021-12-21 07:12:32 +0000

    finish search

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,36 +1,22 @@
 class SearchsController < ApplicationController
   def search
     @customer = current_customer
-    # viewのform_tagにて
-    # 選択したmodelの値を@modelに代入。
+    # viewのform_tagにて 選択したmodelの値を@modelに代入。
     @model = params["model"]
-    # 選択した検索方法の値を@methodに代入。
-    @method = params["method"]
     # 検索ワードを@contentに代入。
     @content = params["content"]
-    # @model, @content, @methodを代入した、
-    # search_forを@recordsに代入。
-    @records = search_for(@model, @content, @method)
+    # @model, @content,を代入した、# search_forを@recordsに代入。
+    @records = search_for(@model, @content)
   end
 
   private
-  def search_for(model, content, method)
-    # 選択したモデルがuserだったら
+  def search_for(model, content)
+     # 選択したモデルがcustomerだったら
     if model == 'customer'
-      # 選択した検索方法がが完全一致だったら
-      if method == 'perfect'
-        Customer.where(name: content, introduction: content)
-      # 選択した検索方法がが部分一致だったら
-      else
-        Customer.where('name LIKE ?', '%'+content+'%')
-      end
-    # 選択したモデルがpostだったら
+     Customer.where('name LIKE ?', '%'+content+'%')
+     # 選択したモデルがbookだったら
     elsif model == 'book'
-      if method == 'perfect'
-        Book.where(title: content)
-      else
-        Book.where('title LIKE ?', '%'+content+'%')
-      end
+     Book.where('title LIKE ?', '%'+content+'%')
     end
   end
 end
