16838fc0c5fff365aa830f133f2b8aa24a33969f
sattaku1538 <choose.chose.chosen@gmail.com>
2022-01-09 06:24:46 +0000

    fix customers validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,10 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   # ↓↓画像
-  attachment :profile_image, destroy: false
   # ↓↓投稿バリデーション
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy, through: :books
@@ -17,7 +14,8 @@ class Customer < ApplicationRecord
   # バリデーション
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :email, presence: true
-  validates :introduction, length: { maximum: 100 }, presence: true
+  validates :introduction, length: { maximum: 200 }, presence: true
+  attachment :profile_image, destroy: false
 
   # 退会機能
   def active_for_authentication?
