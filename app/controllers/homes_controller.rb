ea3e78d8097c60d34af202bfeefc18dc89a7a362
sattaku1538 <choose.chose.chosen@gmail.com>
2022-01-17 04:52:48 +0000

    fix homes top and about

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,8 @@ class HomesController < ApplicationController
   def top
     @customer = current_customer
   end
+  
+  def about
+    @customer = current_customer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get 'homes/about'
   get '/search', to: 'searchs#search'
 
   namespace :admin do
